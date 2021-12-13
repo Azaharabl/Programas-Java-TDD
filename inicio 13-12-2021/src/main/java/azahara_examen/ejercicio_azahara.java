f7daf55f80ecef26ca1b920c6d64ea3abb444804
Azahara Blanco Rodriguez <blancaluna92>
2021-12-13 10:45:00 +0100

    un prollecto vacio para iniciar

---
+++ inicio 13-12-2021/src/main/java/azahara_examen/ejercicio_azahara.java
@@ -0,0 +1,12 @@
+package azahara_examen;
+
+/**
+ *
+ *
+ */
+public class ejercicio_azahara {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ inicio 13-12-2021/src/test/java/azahara_examenTest/ejercicio_azaharaTest.java
@@ -0,0 +1,14 @@
+package azahara_examenTest;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ejercicio_azaharaTest {
+
+
+
+    
+
+
+}
